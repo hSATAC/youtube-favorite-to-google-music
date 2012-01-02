b0cb97a3f892c7c5fb780304b2a2f0a6bf66e12f
hSATAC <hsatac@gmail.com>
2012-01-02 23:36:04 +0800

    init commit

---
+++ config/schedule.rb
@@ -0,0 +1,20 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :output, "/Users/cat/Dropbox/Music/auto_add_google_music/yt2gm.log"
+
+every 15.minutes do
+ command "cd /Users/cat/Dropbox/Music/auto_add_google_music/ && ./yt2gm.rb"
+ #runner "MyModel.some_method"
+ #rake "some:great:rake:task"
+end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
---
+++ yt2gm.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'open-uri'
+require 'json'
+require 'parseconfig'
+
+# read config file 
+config_file = '/Users/cat/Dropbox/Music/auto_add_google_music/config/yt2gm.conf'
+conf = ParseConfig.new(config_file)
+last_check = conf.params['last_check']
+
+# write current time to config
+conf.params['last_check'] = Time.now.utc.to_s
+file = open(conf.config_file, 'w')
+conf.write(file)
+file.close()
+
+#debug
+#last_check = Time.parse("UTC 2011-12-30 13:54:32").utc.to_s
+
+unless last_check.nil? then
+
+  puts 'Last Check Time: '+ last_check
+
+  json_object = JSON.parse(open("http://gdata.youtube.com/feeds/api/users/sevencat777/favorites?alt=json&v=2").read)
+  for item in json_object['feed']['entry']
+    if(Time.parse(last_check) < Time.parse(item['published']['$t'])) then
+      puts item['title']['$t'] + 'added at ' + Time.parse(item['published']['$t']).to_s
+      for link in item['link']
+        if link['rel'] == 'alternate' then
+          puts 'Grabbing: '+ link['href']
+          `youtube-dl -o "%(stitle)s.%(ext)s" -q --extract-audio --audio-format "mp3" "#{link['href']}"`
+        end
+      end
+    end
+  end
+end
+
+
